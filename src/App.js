1cb55bcc96dbf190edcbf523447d4d3018758aa6
berinfo <berinfo@maestralsolutions.com>
2022-02-16 09:56:56 +0100

    expenseitem component added and style

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import ExpenseItem from "./components/ExpenseItem";
+
+function App() {
+  
+  return (
+    <div className="App">
+      <ExpenseItem />
+      
+    </div>
+  );
+}
+
+export default App;

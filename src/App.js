15a28770dd4141a539ee893a6ba85b9b0bc88bb0
berinfo <berinfo@maestralsolutions.com>
2022-02-16 13:52:59 +0100

    added form and useState hooks

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import React from "react";
-import Expenses from "./components/Expenses";
+import Expenses from "./components/Expenses/Expenses";
+import NewExpense from "./components/NewExpense/NewExpense";
 
 function App() {
   const expenses = [
@@ -26,6 +27,7 @@ function App() {
 
   return (
     <div className="App">
+      <NewExpense />
       <Expenses items={expenses} />
     </div>
   );

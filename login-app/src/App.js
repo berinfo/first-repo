59a9966adc38d4cb85de4a4eda96a0803e811464
berinfo <berinfo@maestralsolutions.com>
2022-02-17 22:33:16 +0100

    useContext added

--- login-app/src/App.js
+++ login-app/src/App.js
@@ -1,36 +1,20 @@
-import React, { useState, useEffect } from "react";
+import React, { useContext } from "react";
 
 import Login from "./components/Login/Login";
 import Home from "./components/Home/Home";
 import MainHeader from "./components/MainHeader/MainHeader";
+import AuthContext from "./store/auth-context";
 
 function App() {
-  const [isLoggedIn, setIsLoggedIn] = useState(false);
-
-  useEffect(() => {
-    const user = localStorage.getItem("isLoggedIn");
-    if (user === "1") {
-      setIsLoggedIn(true);
-    }
-  }, []);
-
-  const loginHandler = (email, password) => {
-    localStorage.setItem("isLoggedIn", "1");
-    setIsLoggedIn(true);
-  };
-
-  const logoutHandler = () => {
-    setIsLoggedIn(false);
-  };
-
+  const ctx = useContext(AuthContext);
   return (
-    <React.Fragment>
-      <MainHeader isAuthenticated={isLoggedIn} onLogout={logoutHandler} />
+    <>
+      <MainHeader onLogout={ctx.onLogout} />
       <main>
-        {!isLoggedIn && <Login onLogin={loginHandler} />}
-        {isLoggedIn && <Home onLogout={logoutHandler} />}
+        {!ctx.isLoggedIn && <Login onLogin={ctx.onLoginr} />}
+        {ctx.isLoggedIn && <Home onLogout={ctx.onLogout} />}
       </main>
-    </React.Fragment>
+    </>
   );
 }
 

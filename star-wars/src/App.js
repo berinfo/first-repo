3f3476c0d3d008fbdad51ea7d24a2771f7f1b8d5
berinfo <berinfo@maestralsolutions.com>
2022-02-18 23:13:05 +0100

    usecallback added to prevent infinite loop on first render and enable calling function on click

--- star-wars/src/App.js
+++ star-wars/src/App.js
@@ -1,4 +1,4 @@
-import React, { useState } from "react";
+import React, { useState, useEffect, useCallback } from "react";
 
 import MoviesList from "./components/MoviesList";
 import "./App.css";
@@ -7,7 +7,8 @@ function App() {
   const [movies, setMovies] = useState([]);
   const [loading, setLoading] = useState(false);
   const [error, setError] = useState(null);
-  async function fetchMoviesHandler() {
+
+  const fetchMoviesHandler = useCallback(async () => {
     setError(null);
     setLoading(true);
     try {
@@ -31,7 +32,10 @@ function App() {
       setError(error.message);
     }
     setLoading(false);
-  }
+  }, []);
+  useEffect(() => {
+    fetchMoviesHandler();
+  }, [fetchMoviesHandler]);
   let content = <p>Found no movies.</p>;
   if (movies.length > 0) {
     content = <MoviesList movies={movies} />;

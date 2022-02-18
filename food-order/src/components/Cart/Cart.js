e953d2bf01daccb45787f3303dbf1a0592123e26
berinfo <berinfo@maestralsolutions.com>
2022-02-18 12:12:48 +0100

    create portal and modal added

--- food-order/src/components/Cart/Cart.js
+++ food-order/src/components/Cart/Cart.js
@@ -1,19 +1,26 @@
 import classes from "./Cart.module.css";
+import Modal from "../UI/Modal";
 
 const Cart = () => {
   const cartItems = (
-    <ul>
+    <ul className={classes["cart-items"]}>
       {[{ id: "c1", name: "Sushi", amount: 2, price: 12.99 }].map((item) => (
         <li>{item.name}</li>
       ))}
     </ul>
   );
   return (
-    <div>
+    <Modal>
       {cartItems}
-      <div></div>
-      <div></div>
-    </div>
+      <div className={classes.total}>
+        <span>total amount</span>
+        <span>35.55</span>
+      </div>
+      <div className={classes.actions}>
+        <button className={classes["button--alt"]}>Close</button>
+        <button className={classes.button}>Order</button>
+      </div>
+    </Modal>
   );
 };
 

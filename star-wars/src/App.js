356bce462455a5fa71acd14bd24cc9c528cae9c5
berinfo <berinfo@maestralsolutions.com>
2022-02-18 22:36:49 +0100

    async await used for fetch data

--- star-wars/src/App.js
+++ star-wars/src/App.js
@@ -5,23 +5,19 @@ import "./App.css";
 
 function App() {
   const [movies, setMovies] = useState([]);
-  function fetchMoviesHandler() {
-    fetch("https://swapi.dev/api/films/")
-      .then((response) => {
-        return response.json();
-      })
-      .then((data) => {
-        console.log(data);
-        const transformedMovies = data.results.map((m) => {
-          return {
-            id: m.episode_id,
-            title: m.title,
-            openingText: m.opening_crawl,
-            releaseDate: m.release_date,
-          };
-        });
-        setMovies(transformedMovies);
-      });
+  async function fetchMoviesHandler() {
+    const response = await fetch("https://swapi.dev/api/films/");
+    const data = await response.json();
+
+    const transformedMovies = data.results.map((m) => {
+      return {
+        id: m.episode_id,
+        title: m.title,
+        openingText: m.opening_crawl,
+        releaseDate: m.release_date,
+      };
+    });
+    setMovies(transformedMovies);
   }
 
   return (
